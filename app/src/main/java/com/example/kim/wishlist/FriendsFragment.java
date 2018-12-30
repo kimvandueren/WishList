18e3cc1d8b1e366d993f210c834fe9b7ccdeddf0
Kim van Dueren den Hollander <Kim.van.Dueren.den.Hollander@hva.nl>
2018-12-30 11:33:23 +0100

    Tabs with fragments work

---
+++ app/src/main/java/com/example/kim/wishlist/FriendsFragment.java
@@ -0,0 +1,20 @@
+package com.example.kim.wishlist;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FriendsFragment extends Fragment {
+    private static final String TAG = "Friends";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/kim/wishlist/MainActivity.java
+++ app/src/main/java/com/example/kim/wishlist/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.kim.wishlist;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ListsFragment(), getString(R.string.tab_lists));
+        adapter.addFragment(new FriendsFragment(), getString(R.string.tab_friends));
+        adapter.addFragment(new ProfileFragment(), getString(R.string.tab_profile));
+
+        viewPager.setAdapter(adapter);
     }
 }
